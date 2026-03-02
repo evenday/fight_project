55eb868dfca9ff4c8b60c0477ef0b68f1335d37f
evenday <even4725@naver.com>
2026-03-02 21:40:03 +0900

    CameraZoom 수정
    Checksphere(벽외부로 나가는거 방지)
    ->충돌 될시 distance -= 1 (smoothdamp X -> smoothdamp로 이동시 천천히 이동되어서 외부가 보임)
    
    spherecast
    start: CameraManager위치
    direction: CameraManager -> MainCamera
    MaxDistance: MainCamera + 3.0f 까지
    만약 충돌시
    충돌위치에서 3.0f를 뺀거리를 줌 target으로
    else
    input->mouse_wheel값으로 zoom거리 조정
    
    MouseZoom(), ColZoom() 추가
    각각 입력조작과 충돌시 줌 기능을 가짐
    CameraZoom()에서 사용

--- My project/Assets/Script/CameraController.cs
+++ My project/Assets/Script/CameraController.cs
@@ -5,76 +5,72 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 offset = new Vector3(0, 0, 0);
+    public Vector3 offset = new Vector3(0, 0, 0);   //CameraManager 시작 위치(target.position + offset)
     [Header("Target")]
     public Transform target;
     [Header("Rotation Axis")]
-    public Transform y_axis;        //Y axis Object.transform
-    public Transform x_axis;        //X axis Object.transform
+    public Transform y_axis;                        //Y axis Object.transform
+    public Transform x_axis;                        //X axis Object.transform
     [Header("Camera")]
     public Transform main_camera;
     public float distance = 12;
-    public float camera_velocity = 0.0f;
+    //public float camera_velocity = 0.0f;
     public float rot_sensitiv = 1.0f;
 
     //Zoom
-    float zoom_size = 0.0f;     //줌 거리 target
-    float smooth_time = 0.3f;
-    float current_velocity = 0.0f;
+    int layer_mask;
+    float zoom_pos = 0.0f;                          //Use zoom in/out target position
+    float col_zoom_pos = 0.0f;                      //Use collider check Floor or Wall (enter collision position)
+    float mouse_wheel = 0.0f;                       //Mouse wheel value -> Input 
+    float move_time = 0.3f;                         //Time taken to camera move(SmoothDamp factor)
+    float current_velocity = 0.0f;                  //Current camera move velocity(out value)
 
     //rotation
-    float mouse_wheel = 0.0f;
-    float yaw = 0.0f;                   //right, left
-    float pitch = 0.0f;                 //up, down
+    float mouse_x;
+    float mouse_y;
+    float yaw = 0.0f;                               //right, left
+    float pitch = 0.0f;                             //up, down
 
 
     void Awake()
     {
         //format Setting
-        zoom_size = distance;                                       //set zoom_size(SmoothDamp()-> target)
+        zoom_pos = distance;                                                                        
         main_camera.localPosition = new Vector3(0, 0, -distance);   //Camera distance
 
         //X axis rotation
         pitch += 20.0f;                                             
-        x_axis.localRotation = Quaternion.Euler(pitch, 0, 0);
+        x_axis.localRotation = Quaternion.Euler(pitch, 0, 0);       
 
         //This object position setting
-        transform.position = target.position + offset;              
+        transform.position = target.position + offset;
+
+        //Collider LayerMask Setting
+        layer_mask = LayerMask.GetMask("Floor", "Wall");
 
     }
 
     private void Update()
     {
-        //Zoom in / out
+        //rotation Input
+        mouse_x = Input.GetAxisRaw("Mouse X") * rot_sensitiv;
+        mouse_y = Input.GetAxisRaw("Mouse Y") * rot_sensitiv;
+
+        //Mouse wheel Input
         mouse_wheel = Input.GetAxis("Mouse ScrollWheel");
 
-        
 
     }
-    float col_zoom_size = 12;
+    
     void LateUpdate()
     {
         //Camera 
-        transform.position = target.position + offset;        //Update this.object position 
-        CameraLookTarget();     //Camera Look player
+        transform.position = target.position + offset;          //Update this.object position 
+        CameraLookTarget();                                     //Camera Look player
         CameraRotation();
-        int layer_mask = LayerMask.GetMask("Floor", "Wall");
 
-        Vector3 ray_dir = main_camera.position - transform.position;
-        if (Physics.SphereCast(transform.position, 0.5f, ray_dir, out RaycastHit hit, distance, layer_mask))
-        {
+        CameraZoom();                                           //Camera Zoom
 
-            col_zoom_size = hit.distance;
-            Debug.Log(col_zoom_size);
-            CameraSoothMove(col_zoom_size);
-        }
-        else
-            CameraZoom();           //Camera Zoom
-
-
-        Debug.DrawRay(transform.position, ray_dir, Color.green);
-        //ASFDASF
-        //dasfsadsdfa
     }
 
     
@@ -82,35 +78,59 @@ public class CameraController : MonoBehaviour
     {
         //Camera look target 
         Vector3 camera_dir = transform.position - main_camera.position; //dir camera -> camera_manager
+     
         main_camera.rotation = Quaternion.LookRotation(camera_dir);
         
     }
 
-
-    void CameraSoothMove(float target_dis)
+    void MouseZoom()
     {
-        distance = Mathf.SmoothDamp(distance, target_dis, ref current_velocity, smooth_time);
+        zoom_pos -= mouse_wheel * 10;
+        zoom_pos = Mathf.Clamp(zoom_pos, 2, 15);
+    }
+
+    bool ColZoom()
+    {
+        Vector3 ray_dir = main_camera.position - transform.position;
+        Debug.DrawRay(transform.position, ray_dir.normalized * (distance + 3.0f), Color.green);
+
+        if (Physics.SphereCast(transform.position, 1.0f, ray_dir.normalized, out RaycastHit hit, distance + 3.0f, layer_mask))
+        {
+            col_zoom_pos = hit.distance - 3.0f;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void CameraSoothMove(float target_dis)                              //Apply Camera Move
+    {
+        distance = Mathf.SmoothDamp(distance, target_dis, ref current_velocity, move_time);
+        distance = Mathf.Clamp(distance, 2, 15);
         main_camera.localPosition = new Vector3(0, 0, -distance);
     }
 
     void CameraZoom() 
     {
-        zoom_size  -=  mouse_wheel * 10;
-        zoom_size = Mathf.Clamp(zoom_size, 1, 15);
+        if (Physics.CheckSphere(main_camera.position, 0.5f, layer_mask))
+            distance -= 1.0f;
 
-        CameraSoothMove(zoom_size);
-        
-
+        if (ColZoom())
+        {
+            CameraSoothMove(col_zoom_pos);
+        }
+        else
+        {
+            MouseZoom();
+            CameraSoothMove(zoom_pos);
+        }    
     }
 
 
     void CameraRotation()
     {
-        float mouse_x = Input.GetAxisRaw("Mouse X") * rot_sensitiv;
-        float mouse_y = Input.GetAxisRaw("Mouse Y") * rot_sensitiv;
-
         //상하 카메라 회전 제한
-        pitch = Mathf.Clamp(pitch, -80, 80);
+        pitch = Mathf.Clamp(pitch, -40, 80);
         yaw += mouse_x;
         pitch += mouse_y;
 
